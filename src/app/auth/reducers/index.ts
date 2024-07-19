6f0e7013b350c0931795343c1691a9e24df947d7
Juan Maldonado <juan@nooddle.es>
2024-07-19 10:34:33 +0200

    merge

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,20 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+
+export const authFeatureKey = 'auth';
+
+export interface AppState {
+
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+
+};
+
+
+
